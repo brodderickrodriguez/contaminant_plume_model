35fbd83332fef4f2110ab5e0c1a657daa17d7c09
brodderickrodriguez <bcr@brodderick.com>
2019-07-29 09:53:45 -0500

    resolved filepath issues in jarbuilder

--- Scala-Plume-Model/jar-builder.sh
+++ Scala-Plume-Model/jar-builder.sh
@@ -6,8 +6,7 @@
 E_N=plume-scala   # extension name
 E_D=nlogo-model   # extension directory
 N_V=6.0.4         # NetLogo version
-#N_D=/Applications/NetLogo\ $N_V/NetLogo\ $N_V.app # NetLogo path
-N_D = /Applications/NetLogo-6.0.4
+N_D=/Applications/NetLogo\ $N_V/NetLogo\ $N_V.app # NetLogo path
 
 clear
 echo "starting jar-builder..."
@@ -15,7 +14,7 @@ echo "killing NetLogo (unsafe)..."
 killall NetLogo $N_V    # 'killall' is essentially force quit
 
 echo "removing old compiled files..."
-rm -r -f target
+rm -rf target
 rm -f $E_D/$E_N/$E_N.jar
 rm -f *.jar
 
@@ -27,8 +26,6 @@ mkdir $E_D/$E_N
 mv $E_N.jar $E_D/$E_N
 
 echo "launching NetLogo..."
-# open -a $N_D $E_D/extended-plume.nlogo
-open /Users/Kenan/PycharmProjects/Plume-Model/Scala-Plume-Model/$E_D/plume_extended.nlogo
-#open -a /Applications/NetLogo\ $N_V/NetLogo\ $N_V.app ./$E_D/tests_plume_scala.nlogo
+open -a /Applications/NetLogo\ $N_V/NetLogo\ $N_V.app ./$E_D/plume_extended.nlogo
 
 echo "jar-builder done"
